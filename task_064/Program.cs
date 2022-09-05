6f79beebcb2709ac8e542ee783d6c9664a3fa5fd
CJINBNH <alex_egorov_89@mail.ru>
2022-09-05 17:34:10 +0400

    Программа выводит натуральные числа от N до 1

--- task_064/Program.cs
+++ task_064/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+Console.WriteLine("Введите чило N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int n)
+{
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    PrintNumbers(n-1);
+}
+PrintNumbers(number);
